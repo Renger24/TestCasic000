debbc166ed0a9696579556b8caba8da84cd2cd0e
Renger24 <97744390+Renger24@users.noreply.github.com>
2025-11-01 03:52:08 +0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,10 @@
+// vite.config.ts
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   build: {
-    outDir: 'dist', // Vercel будет деплоить из этой директории
-    target: 'esnext', // Для поддержки современных возможностей
+    outDir: 'dist', // Убедитесь, что это 'dist'
   },
 })
